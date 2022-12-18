36bef454d7c8fab9f3227fd28c1dad3a902f05d6
Chae-G-0 <corkdud@naver.com>
2022-12-18 22:30:07 +0900

    221218

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Mybutton from '../component/Mybutton'
+import MyHeader from '../component/MyHeader'
 
 const New = () => {
+  const navigate = useNavigate()
   return (
-    <div>일기 추가 작성 페이지</div>
-  )
+    <div>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={<Mybutton text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
+      />
+    </div>
+  );
 }
 
 export default New
